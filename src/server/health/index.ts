f8ca3d7458da91ddf0f7e9b1a78edf23397612c7
Tomas Stropus <h.guzas@gmail.com>
2022-05-11 15:01:41 +0300

    Added root route

--- src/server/health/index.ts
+++ src/server/health/index.ts
@@ -2,6 +2,10 @@ import Router from '@koa/router';
 
 export function addHealthRoutes(router: Router) {
 
+    router.get('/', ctx => {
+        ctx.status = 200;
+    });
+
     router.get('/health', ctx => {
         ctx.status = 200;
     });
