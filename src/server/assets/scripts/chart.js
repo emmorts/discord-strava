86dba5eee324ea1c033a5a66be62b2b70a6319e8
Tomas Stropus <h.guzas@gmail.com>
2022-06-02 14:22:06 +0300

    changes

--- src/server/assets/scripts/chart.js
+++ src/server/assets/scripts/chart.js
@@ -1,11 +1,11 @@
 window.drawChart = function (config) {
   const ctx = document.getElementById('monthly-chart').getContext('2d');
 
-  config.options.plugins = {
-    annotation: {
-      annotations: getAnnotations(config.data)
-    }
-  }
+  // config.options.plugins = {
+  //   annotation: {
+  //     annotations: getAnnotations(config.data)
+  //   }
+  // }
 
   if (window.leaderboardType === 3) {
     config.options.scales.y.ticks.precision = 3;
@@ -17,23 +17,57 @@ window.drawChart = function (config) {
     }
   }
 
-  new Chart(ctx, config);
+  window.chart = new Chart(ctx, config);
+
+  loadAnnotations(config.data);
 }
 
-function getAnnotations(data) {
+function loadAnnotations(data) {
   return data.datasets.map(dataset => {
-    const image = new Image();
+    const image = document.createElement('img')
     image.src = dataset.imageUrl;
+    image.onload = () => {
+      if (!window.chart.options.plugins.annotation.annotations.length) {
+        window.chart.options.plugins = {
+          annotation: {
+            annotations: []
+          }
+        };
+      }
 
-    return {
-      type: 'label',
-      drawTime: 'afterDraw',
-      content: image,
-      width: 40,
-      height: 40,
-      xAdjust: -20,
-      xValue: data.labels[data.labels.length - 1],
-      yValue: dataset.data[dataset.data.length - 1]
+      const lastDataPointIndex = getLastExistingDataPointIndex(dataset.data);
+      const closeToLeftBorder = lastDataPointIndex < 5;
+      const closeToRightBorder = lastDataPointIndex > (data.labels.length - 4);
+      const xAdjust = closeToRightBorder
+        ? -20 : closeToLeftBorder
+        ? 22 : 0;
+
+      window.chart.options.plugins.annotation.annotations.push({
+        type: 'label',
+        drawTime: 'afterDraw',
+        content: image,
+        width: 40,
+        height: 40,
+        xAdjust: xAdjust,
+        xValue: data.labels[lastDataPointIndex],
+        yValue: dataset.data[lastDataPointIndex]
+      });
+
+      setTimeout(() => {
+        window.chart.update()
+      }, 0);
     }
   });
+}
+
+function getLastExistingDataPointIndex(data) {
+  let index = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i] !== null) {
+      index = i;
+    }
+  }
+
+  return index;
 }
